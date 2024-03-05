8323b6059c8fefbdb765bd6179ef1e6f990035d0
unknown <ministrov2018@gmail.com>
2024-03-05 11:33:44 +0300

    feat: end up creating AvailableActions class

--- src/classes/AvailableActions.php
+++ src/classes/AvailableActions.php
@@ -12,22 +12,19 @@ class AvailableActions {
   const ACTION_DENY = 'act_deny';
   const ACTION_COMPLETE = 'act_complete';
 
-  const ROLE_PERFORMER = 'performer';
-  const ROLE_CLIENT = 'client';
-
-  private $performerId = null;
-  private $clientId = null;
+  private ?int $performerId;
+  private int $clientId;
 
   private $status = null;
 
   /**
    * AvailableActions strategy constructor
    * @param string $status;
-   * @param int $performedId;
+   * @param int|null $performedId;
    * @param int $clientId;
   */
 
-  public function __construct(string $status, ?int $performedId, int $clientId)
+  public function __construct(string $status, int $clientId, ?int $performedId = null)
   {
     $this->setStatus($status);
 
@@ -35,21 +32,88 @@ class AvailableActions {
     $this->clientId = $clientId;
   }
 
-  public function getStatusMap()
+  /**
+   * 
+   * @return array
+  */
+  public function getStatusMap(): array
   {
     return [
       self::STATUS_NEW => 'Новое',
-      self::STATUS_CANCEL => 'Отмена',
-      self::STATUS_EXPIRED => '',
-      self::STATUS_COMPLETE => '',
-      self::STATUS_IN_PROGRESS => '',
+      self::STATUS_CANCEL => 'Отменено',
+      self::STATUS_EXPIRED => 'Провалено',
+      self::STATUS_COMPLETE => 'Выполнено',
+      self::STATUS_IN_PROGRESS => 'В работе',
     ];
   }
 
-  public function setStatus($status)
+  /**
+   * @return array
+  */
+  public function getActionsMap(): array
   {
-    $status = $this->status;
+    return [
+      self::ACTION_CANCEL => 'Отменить',
+      self::ACTION_RESPONSE => 'Откликнуться',
+      self::ACTION_COMPLETE => 'Выполнено',
+      self::ACTION_DENY => 'Отказаться'
+    ];
+  }
 
-    return $status;
+  /**
+   * @param string $action
+   * 
+   * @return string|null
+  */
+  public function getNextStatus(string $action): ?string
+  {
+    $map = [
+      self::ACTION_COMPLETE => self::ACTION_COMPLETE,
+      self::ACTION_CANCEL => self::ACTION_CANCEL,
+      self::ACTION_DENY => self::ACTION_CANCEL
+    ];
+
+    return $map[$action] ?? null;
+
+    // if (isset($map[$action])) {
+    //   return $map[$action];
+    // } else {
+    //   return null;
+    // }
+  }
+
+  /**
+   * @param mixed $status
+   * 
+   * @return [type]
+  */
+  public function setStatus($status): void
+  {
+    $availableStatus = [
+      self::STATUS_NEW, 
+      self::STATUS_CANCEL, 
+      self::STATUS_COMPLETE, 
+      self::STATUS_EXPIRED, 
+      self::STATUS_IN_PROGRESS
+    ];
+
+    if (is_array($status, $availableStatus)) {
+      $this->status = $status;
+    }
+  }
+
+  /**
+   * Возвращает действия, доступные для указанного статуса
+   * @param string $status
+   * @return array
+  */
+  private function setAllowedStatus(string $status): array
+  {
+    $map = [
+      self::STATUS_IN_PROGRESS => [self::ACTION_DENY, self::ACTION_COMPLETE],
+      self::STATUS_NEW => [self::ACTION_CANCEL, self::ACTION_RESPONSE]
+    ];
+
+    return $map[$status] ?? [];
   }
 }
