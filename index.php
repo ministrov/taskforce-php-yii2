12ebb9d0be97cd2eaa2492cbd8188bff17454276
unknown <ministrov2018@gmail.com>
2024-03-06 14:37:11 +0300

    fix: end up fixing errors with namespaces in index.php, and refresh vendor folder

--- index.php
+++ index.php
@@ -4,28 +4,12 @@ ini_set('assert.exception', 1);
 
 use taskforce\logic\AvailableActions;
 
-use taskforce\greetings\Greeting;
-
 $strategy = new AvailableActions(AvailableActions::STATUS_NEW, 3, 1);
 
-var_dump('new -> performer', $strategy->getStatusMap());
-var_dump('new -> client,alien', $strategy->getStatusMap());
-var_dump('new -> client,same', $strategy->getStatusMap());
+// var_dump('new -> performer', $strategy->getStatusMap());
+// var_dump('new -> client,alien', $strategy->getStatusMap());
+// var_dump('new -> client,same', $strategy->getStatusMap());
 
-var_dump('proceed -> performer,same', $strategy->getStatusMap());
+// var_dump('proceed -> performer,same', $strategy->getStatusMap());
 
-assert($strategy->getNextStatus(AvailableActions::ACTION_CANCEL) == AvailableActions::STATUS_CANCEL, 'cancel action');
-
-print_r($strategy);
-
-print('<br>');
-
-$greeting = new Greeting("Hello World!");
-
-echo $greeting->getGreeting("Greeting");
-
-print_r($greeting);
-
-$greeting_2 = new Greeting('How is it going?');
-
-print_r($greeting_2);
+// assert($strategy->getNextStatus(AvailableActions::ACTION_CANCEL) == AvailableActions::STATUS_CANCEL, 'cancel action');
