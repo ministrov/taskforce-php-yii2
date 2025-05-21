652f0b170dc43c57d082e3fa73c02d3027097382
Zhila <ministrov2018@gmail.com>
2025-05-21 13:31:46 +0300

    fix - Clean index file from print function

--- index.php
+++ index.php
@@ -4,11 +4,11 @@ ini_set('assert.exception', 1);
 
 use taskforce\logic\AvailableActions;
 
-use taskforce\concrete\ConcreteClass1;
+// use taskforce\concrete\ConcreteClass1;
 
-use taskforce\greetings\Greeting;
+// use taskforce\greetings\Greeting;
 
-use taskforce\product\Product;
+// use taskforce\product\Product;
 
 // $timeNow = new DateTime();
 
@@ -32,9 +32,6 @@ $strategy = new AvailableActions(AvailableActions::STATUS_NEW, 3, 1);
 
 // print_r($how_it_going->getGreeting("How is it going!"));
 
-$new_greeting = new Greeting("We love to eat a lot!!!))");
-echo $new_greeting->getGreeting('sdfdf');
-
 // print("<br>");
 
 // print_r($hello_world);
@@ -46,78 +43,3 @@ echo $new_greeting->getGreeting('sdfdf');
 // print("<br>");
 
 // echo $how_it_going->getGreeting("How is it going!");
-
-$class1 = new ConcreteClass1;
-$class1->printOut();
-echo $class1->prefixValue('FOO_') . "\n";
-
-// class Product
-// {
-//   public string $name;
-//   public string $price;
-
-
-//   function __construct(string $name, float $price)
-//   {
-//     $this->name = $name;
-//     $this->price = $price;
-//   }
-
-//   public function getInfo()
-//   {
-//     return "Product: {$this->name}, Price: {$this->price}";
-//   }
-// }
-
-// $product = new Product('Notebook', 888.8);
-// echo $product->getInfo();
-
-// class FormValidator
-// {
-//   public $formData = [];
-//   public $requiredFields = [];
-
-//   private $errors = [];
-
-//   public function __construct($formData, $requiredFields)
-//   {
-//     $this->formData = $formData;
-//     $this->requiredFields = $requiredFields;
-//   }
-
-//   public function validate()
-//   {
-//     $fields = array_merge($this->requiredFields, array_keys($this->formData));
-//     $errors = [];
-
-//     foreach ($fields as $field) {
-//       $errors[$field] = $this->validateFilled($field);
-//     }
-
-//     $this->errors = array_filter($errors);
-
-//     return empty($this->errors);
-//   }
-
-//   public function getErrors()
-//   {
-//     return $this->errors;
-//   }
-
-//   public function validateFilled($name)
-//   {
-//     if (empty($this->formData[$name])) {
-//       return "Это поле должно быть заполнено";
-//     }
-
-//     return null;
-//   }
-// }
-
-// $formValidator = new FormValidator($_POST, ['title', 'description']);
-
-// $isFormValid = $formValidator->validate();
-
-// if (!$isFormValid) {
-//   $errors = $formValidator->getErrors();
-// }
