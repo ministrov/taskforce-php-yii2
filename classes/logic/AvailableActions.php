a594b7167bb0437d3ac059803c5c4140c1b88c3e
Zhila <ministrov2018@gmail.com>
2025-05-22 10:05:26 +0300

    fix - Fixing an constructor method in AvailableActions class

--- classes/logic/AvailableActions.php
+++ classes/logic/AvailableActions.php
@@ -1,40 +1,43 @@
 <?php
+
 namespace taskforce\logic;
 
 use DateTime;
 
-class AvailableActions {
+class AvailableActions
+{
   const STATUS_NEW = 'new';
   const STATUS_IN_PROGRESS = 'proceed';
   const STATUS_CANCEL = 'cancel';
   const STATUS_COMPLETE = 'complete';
   const STATUS_EXPIRED = 'exired';
-  
+
   const ACTION_RESPONSE = 'act_response';
   const ACTION_CANCEL = 'act_cancel';
   const ACTION_DENY = 'act_deny';
   const ACTION_COMPLETE = 'act_complete';
 
-  const ROLE_CUSTOMER = 'customer';
   const ROLE_PERFORMER = 'performer';
+  const ROLE_CLIENT = 'customer';
 
   private ?int $performerId;
   private int $clientId;
 
   private $status = null;
+  private $finishDate = null;
 
   /**
    * AvailableActions strategy constructor
    * @param string $status;
    * @param int|null $performedId;
    * @param int $clientId;
-  */
+   */
 
-  public function __construct(string $status, int $clientId, ?int $performedId = null)
+  public function __construct(string $status, ?int $performerId, int $clientId)
   {
     $this->setStatus($status);
 
-    $this->performerId = $performedId;
+    $this->performerId = $performerId;
     $this->clientId = $clientId;
   }
 
@@ -56,7 +59,7 @@ class AvailableActions {
   /**
    * Возвращает массив необходимых действий
    * @return array
-  */
+   */
   public function getActionsMap(): array
   {
     return [
@@ -71,7 +74,7 @@ class AvailableActions {
    * 
    * @param string $action
    * @return string|null
-  */
+   */
   public function getNextStatus(string $action): ?string
   {
     $map = [
@@ -93,14 +96,14 @@ class AvailableActions {
    * @param mixed $status
    * 
    * @return [type]
-  */
+   */
   public function setStatus($status): void
   {
     $availableStatus = [
-      self::STATUS_NEW, 
-      self::STATUS_CANCEL, 
-      self::STATUS_COMPLETE, 
-      self::STATUS_EXPIRED, 
+      self::STATUS_NEW,
+      self::STATUS_CANCEL,
+      self::STATUS_COMPLETE,
+      self::STATUS_EXPIRED,
       self::STATUS_IN_PROGRESS
     ];
 
@@ -112,17 +115,14 @@ class AvailableActions {
   /**
    * Возвращает действия доступные для каждой роли
    * @return array
-  */
-  private function roleAllowedActions()
-  {
-
-  }
+   */
+  private function roleAllowedActions() {}
 
   /**
    * Возвращает действия, доступные для каждого статуса статуса
    * @param string $status
    * @return array
-  */
+   */
   private function statusAllowedActions(): array
   {
     $map = [
@@ -139,7 +139,7 @@ class AvailableActions {
   /**
    * Возвращает массив необходимых статусов 
    * @return array
-  */
+   */
   private function getStatusMap(): array
   {
     return [
