bfe64a36daef59f9524661574b9dac663563291c
Zhila <ministrov2018@gmail.com>
2025-05-26 13:35:49 +0300

    fix - Modify formValidator static property of name

--- index.php
+++ index.php
@@ -8,13 +8,14 @@ use taskforce\validators\FormValidator;
 $strategy = new AvailableActions(AvailableActions::STATUS_NEW, 3, 1);
 $formValidator = new FormValidator();
 
-var_dump('new -> performer', $strategy->getAvailableActions(AvailableActions::ROLE_PERFORMER, 2)) . '<br/>';
-var_dump('new -> client,alien', $strategy->getAvailableActions(AvailableActions::ROLE_CLIENT, 2)) . '<br/>';
-var_dump('new -> client,same', $strategy->getAvailableActions(AvailableActions::ROLE_CLIENT, 1)) . '<br/>';
+var_dump('new -> performer', $strategy->getAvailableActions(AvailableActions::ROLE_PERFORMER, 2));
+var_dump('new -> client,alien', $strategy->getAvailableActions(AvailableActions::ROLE_CLIENT, 2));
+var_dump('new -> client,same', $strategy->getAvailableActions(AvailableActions::ROLE_CLIENT, 1));
 
-var_dump('proceed -> performer,same', $strategy->getAvailableActions(AvailableActions::ROLE_PERFORMER, 3)) . '<br/>';
+var_dump('proceed -> performer,same', $strategy->getAvailableActions(AvailableActions::ROLE_PERFORMER, 3));
 
-var_dump($formValidator->formData);
+print_r($formValidator->formData);
+echo FormValidator::$name = 'static';
 
 assert($strategy->getNextStatus(AvailableActions::ACTION_CANCEL) == AvailableActions::STATUS_CANCEL, 'cancel action');
 
