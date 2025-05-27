c69a76b7a4a1ed28e0eacbcf80e0e86e5daf1f39
Zhila <ministrov2018@gmail.com>
2025-05-27 17:15:12 +0300

    feature - Create a new events table for taskforce database

--- taskforce.sql
+++ taskforce.sql
@@ -39,3 +39,21 @@ CREATE TABLE `cities` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for events
+-- ----------------------------
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE `events` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `task_id` int(11) unsigned NOT NULL,
+  `type` char(12) NOT NULL,
+  `dt_add` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_read` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `fk_events_users_1` (`user_id`),
+  KEY `fk_events_tasks_1` (`task_id`),
+  CONSTRAINT `fk_events_tasks_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`id`),
+  CONSTRAINT `fk_events_users_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
