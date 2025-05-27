be843a1ab1c5ba7d5d35675ace67ed01f5ca06bc
Zhila <ministrov2018@gmail.com>
2025-05-27 12:57:31 +0300

    feature - Create a new action class called DenyAction

--- classes/abstract/AbstractAction.php
+++ classes/abstract/AbstractAction.php
@@ -1,14 +1,5 @@
 <?php 
 
-// namespace taskforce\abstract;
-
-// abstract class AbstractAction
-// {
-//   abstract public static function getLabel();
-//   abstract public static function getIntervalName();
-//   abstract public static function checkRights($userId, $performerId, $clientId);
-// }
-
 namespace taskforce\abstract;
 
 abstract class AbstractAction
