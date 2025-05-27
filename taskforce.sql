5d1feddc16b9ca1b4dcb579244825787c6ebb792
Zhila <ministrov2018@gmail.com>
2025-05-27 14:51:35 +0300

    feature - Create a taskforce sql file to start add a tables to MySQL database

---
+++ taskforce.sql
@@ -0,0 +1,14 @@
+-- ----------------------------
+-- Table structure for bookmarks
+-- ----------------------------
+DROP TABLE IF EXISTS `bookmarks`;
+CREATE TABLE `bookmarks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `performer_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_bookmarks_users_1` (`user_id`),
+  KEY `fk_bookmarks_users_2` (`performer_id`),
+  CONSTRAINT `fk_bookmarks_users_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_bookmarks_users_2` FOREIGN KEY (`performer_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
